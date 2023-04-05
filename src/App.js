66c2f9d99ac8f5feb8cc1afff9486531b32af0bc
Md Saifulla Jubair <101522775+SaifullaJubair@users.noreply.github.com>
2023-04-05 14:10:34 +0600

    create routes and scroll up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import ScrollToTop from 'react-scroll-up';
+import { Toaster } from 'react-hot-toast';
+
+import { BsArrowUpCircleFill } from 'react-icons/bs';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
+
 
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
+
+      <ScrollToTop showUnder={160} >
+        <span className='text-4xl text-blue-600 hover:text-sky-600'><BsArrowUpCircleFill className='animate-bounce'></BsArrowUpCircleFill></span>
+      </ScrollToTop>
     </div>
   );
 }
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import UnKnownRoutes from '../UnknoownRoutes/UnknownRoutes';
+import Main from '../../Layout/Main/Main';
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import Login from '../../Pages/Login/Login';
+import Register from '../../Pages/Register/Register';
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <Main></Main>,
+      errorElement: <UnKnownRoutes></UnKnownRoutes>,
+      children: [
+         {
+            path: '/',
+            element: <Home></Home>
+         },
+
+
+         {
+            path: '/login',
+            element: <Login></Login>
+         },
+         {
+            path: '/register',
+            element: <Register></Register>
+         },
+
+      ]
+   },
+
+
+])
+
+export default router;
